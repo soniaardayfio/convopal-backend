a9c5c3bdacd60afca329df73a2f676e4e473edb7
Emmanuel Ardayfio <soniaardayfio@gmail.com>
2025-07-23 08:48:29 +0000

    Create FileStorageService.java

---
+++ src/main/java/com/convopal_api/convopal_api/service/FileStorageService.java
@@ -0,0 +1,31 @@
+package com.convopal_api.convopal_api.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.UUID;
+
+@Service
+public class FileStorageService {
+
+    @Value("${file.upload-dir}")
+    private String uploadDir;
+
+    public String storeFile(MultipartFile file) throws IOException {
+        String fileName = UUID.randomUUID() + "_" + file.getOriginalFilename();
+        Path uploadPath = Paths.get(uploadDir);
+
+        if (!Files.exists(uploadPath)) {
+            Files.createDirectories(uploadPath);
+        }
+
+        Path filePath = uploadPath.resolve(fileName);
+        Files.copy(file.getInputStream(), filePath);
+
+        return fileName;
+    }
+}
