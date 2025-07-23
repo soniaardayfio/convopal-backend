d75f2d00ffa7dc9fc5014ba171a26620275d047c
Emmanuel Ardayfio <soniaardayfio@gmail.com>
2025-07-23 08:27:59 +0000

    initialize spring boot

---
+++ src/main/java/com/convopal_api/convopal_api/ConvopalApiApplication.java
@@ -0,0 +1,13 @@
+package com.convopal_api.convopal_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConvopalApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConvopalApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/convopal_api/convopal_api/ConvopalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.convopal_api.convopal_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConvopalApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
